5c48cb467f5ebd69a98d2831d10e6af71b80412b
jimmyzzxhlh <jimmyzzxhlh@users.noreply.github.com>
2017-03-22 21:09:03 -0700

    Convert to maven project

--- src/aurora/AuroraConnection.java
+++ src/aurora/AuroraConnection.java
@@ -1,10 +1,13 @@
 package aurora;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class AuroraConnection {
 	
 	private static Connection connection;
+	private static final String JDBC_URL = "jdbc:mysql://minion.cluster-cemz9y8rpyxp.us-west-2.rds.amazonaws.com:3306/symbol?"
+			+ "user=zzxhlh&password=smyxayg9";
 	
 	private AuroraConnection() {
 		
@@ -17,9 +20,9 @@ public class AuroraConnection {
 	
 	private static void createConnection() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(JDBC_URL);
 		}
-		catch (ClassNotFoundException e) {
+		catch (Exception e) {
 		
 		}
 	}
