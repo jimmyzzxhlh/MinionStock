3eaa8192b0e2e771b149ef37ca4beb9bfd98e6fb
jimmyzzxhlh <jimmyzzxhlh@users.noreply.github.com>
2017-04-04 21:20:19 -0700

    Add log4j support

--- src/aurora/AuroraConnection.java
+++ src/aurora/AuroraConnection.java
@@ -6,9 +6,7 @@ import java.sql.DriverManager;
 public class AuroraConnection {
 	
 	private static Connection connection;
-	private static final String JDBC_URL = "jdbc:mysql://minion.cluster-cemz9y8rpyxp.us-west-2.rds.amazonaws.com:3306/symbol?"
-			+ "user=zzxhlh&password=smyxayg9";
-	
+
 	private AuroraConnection() {
 		
 	}
@@ -20,7 +18,7 @@ public class AuroraConnection {
 	
 	private static void createConnection() {
 		try {
-			connection = DriverManager.getConnection(JDBC_URL);
+			connection = DriverManager.getConnection(AuroraConst.JDBC_URL);
 		}
 		catch (Exception e) {
 		
--- src/aurora/AuroraConst.java
+++ src/aurora/AuroraConst.java
@@ -1,7 +1,13 @@
 package aurora;
 
 public class AuroraConst {
-	public static final String DATABASE_ENDPOINT = "";
-	public static final int DATABASE_PORT = 3306;
+	public static final String ENDPOINT = "minion.cluster-cemz9y8rpyxp.us-west-2.rds.amazonaws.com";
+	public static final int PORT = 3306;
+	public static final String USERNAME = "zzxhlh";
+	public static final String PASSWORD = "smyxayg9";
+	public static final String DATABASE_SYMBOL = "symbol";
+	
+	public static final String JDBC_URL = "jdbc:mysql://" + ENDPOINT + ":" + PORT + "/" +  DATABASE_SYMBOL + "?"
+			+ "user=" + USERNAME + "&password" + PASSWORD;
 	
 }
